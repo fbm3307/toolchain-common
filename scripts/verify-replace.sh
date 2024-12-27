2b74d5c4cdd49cf620da55fecf77271a3e657b28
Feny Mehta <fbm3307@gmail.com>
2024-12-27 20:01:59 +0530

    sh
    Signed-off-by: Feny Mehta <fbm3307@gmail.com>

--- scripts/verify-replace.sh
+++ scripts/verify-replace.sh
@@ -6,7 +6,6 @@ GH_BASE_URL_CRT=https://github.com/codeready-toolchain/
 declare -a REPOS=("${GH_BASE_URL_KS}ksctl" "${GH_BASE_URL_CRT}host-operator" "${GH_BASE_URL_CRT}member-operator" "${GH_BASE_URL_CRT}registration-service" "${GH_BASE_URL_CRT}toolchain-e2e" )
 C_PATH=${PWD}
 ERRORREPOLIST=()
-ERRORFILELIST=()
 
 echo Initiating verify-replace on dependent repos
 for repo in "${REPOS[@]}"
@@ -17,7 +16,6 @@ do
     echo                                                                     
     echo =========================================================================================                                            
     repo_path=${BASE_REPO_PATH}/$(basename ${repo})
-    ERRFILE=$(mktemp ${TMP_DIR}stderrfile.XXX)
     echo "Cloning repo in /tmp"
     git clone --depth=1 ${repo} ${repo_path}
     echo "Repo cloned successfully"
@@ -28,12 +26,7 @@ do
     fi
     echo "Initiating 'go mod replace' of current toolchain common version in dependent repos"
     go mod edit -replace github.com/codeready-toolchain/toolchain-common=${C_PATH}
-    make verify-dependencies 2> >(tee "${ERRFILE}")
-    rc=$?
-    if [ ${rc} -ne 0 ]; then
-    ERRORREPOLIST+="($(basename ${repo}))" 
-    ERRORFILELIST+=("${ERRFILE}")
-    fi
+    make verify-dependencies || ERRORLIST+="($(basename ${repo}))"
     echo                                                          
     echo =========================================================================================
     echo                                                           
@@ -43,10 +36,6 @@ if [ ${#ERRORREPOLIST[@]} -ne 0 ]; then
     for e in ${ERRORREPOLIST[*]}
     do
         echo "${e}"
-    done
-    for c in ${ERRORFILELIST[*]}
-    do
-        cat "${c}"
     done
     exit 1
 else
