37c376f8c2af03b2fb7b927356fbe4b12e7a1eaf
Feny Mehta <fbm3307@gmail.com>
2024-12-27 18:30:13 +0530

    comment
    Signed-off-by: Feny Mehta <fbm3307@gmail.com>

--- scripts/verify-replace.sh
+++ scripts/verify-replace.sh
@@ -3,9 +3,10 @@ TMP_DIR=/tmp/
 BASE_REPO_PATH=$(mktemp -d ${TMP_DIR}replace-verify.XXX)
 GH_BASE_URL_KS=https://github.com/kubesaw/
 GH_BASE_URL_CRT=https://github.com/codeready-toolchain/
-declare -a REPOS=("${GH_BASE_URL_KS}ksctl" "${GH_BASE_URL_CRT}host-operator" "${GH_BASE_URL_CRT}member-operator" "${GH_BASE_URL_CRT}registration-service" "${GH_BASE_URL_CRT}toolchain-e2e")
+declare -a REPOS=("${GH_BASE_URL_KS}ksctl" "${GH_BASE_URL_CRT}registration-service" )
 C_PATH=${PWD}
-ERRORLIST=()
+ERRORREPOLIST=()
+ERRORFILELIST=()
 
 echo Initiating verify-replace on dependent repos
 for repo in "${REPOS[@]}"
@@ -16,26 +17,36 @@ do
     echo                                                                     
     echo =========================================================================================                                            
     repo_path=${BASE_REPO_PATH}/$(basename ${repo})
+    ERRFILE=$(mktemp ${TMP_DIR}stderrfile.XXX)
     echo "Cloning repo in /tmp"
     git clone --depth=1 ${repo} ${repo_path}
     echo "Repo cloned successfully"
     cd ${repo_path}
     if ! make pre-verify; then
-        ERRORLIST+="($(basename ${repo}))"
+        ERRORREPOLIST+="($(basename ${repo}))"
         continue
     fi
     echo "Initiating 'go mod replace' of current toolchain common version in dependent repos"
     go mod edit -replace github.com/codeready-toolchain/toolchain-common=${C_PATH}
-    make verify-dependencies || ERRORLIST+="($(basename ${repo}))"
+    make verify-dependencies 2> >(tee "${ERRFILE}")
+    rc=$?
+    if [ ${rc} -ne 0 ]; then
+    ERRORREPOLIST+="($(basename ${repo}))" 
+    ERRORFILELIST+=("${ERRFILE}")
+    fi
     echo                                                          
     echo =========================================================================================
     echo                                                           
 done
-if [ ${#ERRORLIST[@]} -ne 0 ]; then
+if [ ${#ERRORREPOLIST[@]} -ne 0 ]; then
     echo "Below are the repos with error: "
-    for e in ${ERRORLIST[*]}
+    for e in ${ERRORREPOLIST[*]}
     do
         echo "${e}"
+    done
+    for c in ${ERRORFILELIST[*]}
+    do
+        cat "${c}"
     done
     exit 1
 else
